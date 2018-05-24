7b0b111910a849e324cc466508abf8506627321c
Aluno <Aluno@UPDEVLAB0533>
2018-05-23 22:09:18 -0300

    SAFADITA

---
+++ ProjetoClinica/src/com/up/clinica/model/TipoAnimalDAO.java
@@ -0,0 +1,39 @@
+package com.up.clinica.model;
+
+import java.util.List;
+
+import com.up.clinica.model.dao.IGenericDAO;
+
+public class TipoAnimalDAO implements IGenericDAO<TipoAnimal, String>{
+
+	@Override
+	public List<TipoAnimal> listar() throws Exception {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public TipoAnimal buscar(String id) throws Exception {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void remover(String id) throws Exception {
+		// TODO Auto-generated method stub
+		return;
+	}
+
+	@Override
+	public void atualizar(TipoAnimal objeto) throws Exception {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void persistir(TipoAnimal objeto) throws Exception {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
